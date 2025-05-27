dfc52e795c04ebf4427c436cb20569d996dde139
muy31 <muyibola.aghedo@gmail.com>
2025-05-27 19:24:33 -0400

    Field generation and display

---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+import components.SoccerField;
+
+public class Runner {
+    public static void main(String[] args) {
+        SoccerField field = new SoccerField();
+        field.DisplaySoccerField();
+    }
+}
